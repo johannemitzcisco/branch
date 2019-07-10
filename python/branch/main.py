e00ee09cc1089bdd319db125043fdf3d81099a38
Johan Nemitz <jnemitz@cisco.com>
2019-07-10 14:25:28 -0700

    Demo ready
    Signed-off-by: Johan Nemitz <jnemitz@cisco.com>

--- python/branch/main.py
+++ python/branch/main.py
@@ -2,68 +2,44 @@
 import ncs
 from ncs.application import Service
 
-
-# ------------------------
-# SERVICE CALLBACK EXAMPLE
-# ------------------------
-class ServiceCallbacks(Service):
-
-    # The create() callback is invoked inside NCS FASTMAP and
-    # must always exist.
+class BranchService(Service):
     @Service.create
     def cb_create(self, tctx, root, service, proplist):
         self.log.info('Service create(service=', service._path, ')')
 
-        vars = ncs.template.Variables()
-        vars.add('DUMMY', '127.0.0.1')
-        template = ncs.template.Template(service)
-        template.apply('branch-template', vars)
+        for device in service.device:
+            self.log.info('Registering Device ({}) with PnP (ID: {})'.format(device.nso_name, \
+                device.pnp_id))
+            vars = ncs.template.Variables()
+            template = ncs.template.Template(device)
+            template.apply('service-pnp-device', vars)
 
-    # The pre_modification() and post_modification() callbacks are optional,
-    # and are invoked outside FASTMAP. pre_modification() is invoked before
-    # create, update, or delete of the service, as indicated by the enum
-    # ncs_service_operation op parameter. Conversely
-    # post_modification() is invoked after create, update, or delete
-    # of the service. These functions can be useful e.g. for
-    # allocations that should be stored and existing also when the
-    # service instance is removed.
+            if root.pnp_state.device.exists(device.pnp_id):
+                self.log.info("Device ({}) exists in PnP with state: {}".format(device.name, \
+                    root.pnp_state.device[device.pnp_id].synced))
+                if root.pnp_state.device[device.pnp_id].synced \
+                 and root.devices.device.exists(device.nso_name):
+                    for rolename in device.role:
+                        self.log.info("Applying role ({})".format(rolename))                        
+                        role = root.device_role[rolename]
+                        for templatename in role.template:
+                            self.log.info('Apply Template ({}) to device ({})'.format(templatename, \
+                                device.name))
+                            input = root.devices.device[device.nso_name].apply_template.get_input()
+                            input.template_name = templatename
+                            root.devices.device[device.nso_name].apply_template(input)
+            else:
+                kick_monitor_node = ("/pnp-state/device[serial='{}']/synced").format(device.pnp_id)
+                self.log.info('Creating Kicker Monitor on: ', kick_monitor_node)
+                kicker = root.kickers.data_kicker.create('branch-device-{}'.format(device.name))
+                kicker.monitor = kick_monitor_node
+                kicker.kick_node = ("/branch[name='{}']").format(service.name)
+                kicker.action_name = 'reactive-re-deploy'
 
-    # @Service.pre_lock_create
-    # def cb_pre_lock_create(self, tctx, root, service, proplist):
-    #     self.log.info('Service plcreate(service=', service._path, ')')
-
-    # @Service.pre_modification
-    # def cb_pre_modification(self, tctx, op, kp, root, proplist):
-    #     self.log.info('Service premod(service=', kp, ')')
-
-    # @Service.post_modification
-    # def cb_post_modification(self, tctx, op, kp, root, proplist):
-    #     self.log.info('Service premod(service=', kp, ')')
-
-
-# ---------------------------------------------
-# COMPONENT THREAD THAT WILL BE STARTED BY NCS.
-# ---------------------------------------------
 class Main(ncs.application.Application):
     def setup(self):
-        # The application class sets up logging for us. It is accessible
-        # through 'self.log' and is a ncs.log.Log instance.
         self.log.info('Main RUNNING')
-
-        # Service callbacks require a registration for a 'service point',
-        # as specified in the corresponding data model.
-        #
-        self.register_service('branch-servicepoint', ServiceCallbacks)
-
-        # If we registered any callback(s) above, the Application class
-        # took care of creating a daemon (related to the service/action point).
-
-        # When this setup method is finished, all registrations are
-        # considered done and the application is 'started'.
+        self.register_service('branch-servicepoint', BranchService)
 
     def teardown(self):
-        # When the application is finished (which would happen if NCS went
-        # down, packages were reloaded or some error occurred) this teardown
-        # method will be called.
-
         self.log.info('Main FINISHED')
